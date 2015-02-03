9cf15f3687333af17bc04c3f507cc85213a25154
Ava Burnham <eburnham@umich.edu>
2015-02-03 18:22:46 -0500

    paperclip updated

--- Gemfile
+++ Gemfile
@@ -11,7 +11,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'bootstrap-sass'
 gem 'devise'
-gem 'paperclip'
+gem 'paperclip', '~> 3.0'
 group :development, :test do
      gem 'sqlite3'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  # a user has many pins. Part of the associations thing 
-  
-  has_many :pins       
+
+  has_many :pins, dependent: :destroy
+
+  validates :name, presence: true
 end
